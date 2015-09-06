4ccad3dce038920984c15621e55464b0d225294f
Tom <redsoxfantom@gmail.com>
2015-09-06 14:33:11 -0400

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ my_http = require("http");
 my_http.createServer(function(request,response){
     sys.puts("I got kicked");
     response.writeHeader(200, {"Content-Type": "text/plain"});
-    response.write("Server is up");
+    response.write("Server is running");
     response.end();
 }).listen(12102);
 
