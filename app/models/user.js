475b64466fc89fdb1983d67ae759b1fac3599777
Tom <redsoxfantom@gmail.com>
2015-10-06 19:34:55 -0400

    removed twitter and google fields from user class

--- app/models/user.js
+++ app/models/user.js
@@ -15,20 +15,7 @@ var userSchema = mongoose.Schema({
         token        : String,
         email        : String,
         name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
     }
-
 });
 
 // methods ======================
