65d62170f9c2b9db52f0ca65621cb2fe62cfba21
Tom <redsoxfantom@gmail.com>
2015-10-06 19:42:13 -0400

    removed google / twitter auth

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '875476195881586', // your App ID
         'clientSecret'  : 'ea01312ac4c7d2749b9178d57ff0e88c', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
-    },
-
-    'twitterAuth' : {
-        'consumerKey'       : 'TedCFfTQzP1Mi92KBWr5chxJb',
-        'consumerSecret'    : 'diowgLSJJXNh1vHnMMMgrzIlijJg1Y36Fr9vZEsFS18nKUd4rA',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
-    },
-
-    'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'http://ec2-54-210-134-202.compute-1.amazonaws.com:12102/auth/facebook/callback'
     }
-
 };
