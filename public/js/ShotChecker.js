90ec35ad9a084573a5a7c9b859e4cfb63af56596
Tom <redsoxfantom@gmail.com>
2015-11-01 18:43:43 -0500

    Loop through all ships and loop through their length

--- public/js/ShotChecker.js
+++ public/js/ShotChecker.js
@@ -2,5 +2,21 @@
 // Return true if shot hit something, false otherwise
 function CheckForHit(shot_x, shot_y, ship_list)
 {
+	for(idx = 0; idx < ship_list.length; idx++)
+	{
+		var ship = ship_list[idx];
+		var ship_pos_x = ship.pos_x;
+		var ship_pos_y = ship.pos_y;
+		
+		for(ship_idx = 0; ship_idx < ship.length; ship_idx++)
+		{
+			if(ship_pos_x == shot_x && ship_pos_y == shot_y)
+			{
+				return true;
+			}
+			
+		}
+	}
 	
+	return false;
 }
